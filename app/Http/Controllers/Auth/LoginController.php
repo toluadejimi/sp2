e714a6574f9a0e715787809a7bf3641612dc2f47
Jimmy <toluadejimi@gmail.com>
2025-03-23 18:19:49 +0100

    beneficary added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -78,6 +78,8 @@ class LoginController extends Controller
         $status = $var->status ?? null;
         $message = $var->message ?? "Something went wrong";
 
+        dd($message, $token);
+
 
         if (Auth::attempt($phone_no)) {
 
